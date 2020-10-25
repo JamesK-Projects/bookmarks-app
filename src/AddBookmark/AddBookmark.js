bba72829b0a2e797b8b74212c6ac6802f0a84b68
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-10-25 10:44:28 -0600

    initial upload

--- src/AddBookmark/AddBookmark.js
+++ src/AddBookmark/AddBookmark.js
@@ -1,32 +1,143 @@
-import React, { Component } from 'react';
+import React, { Component } from  'react';
+import BookmarksContext from '../BookmarksContext';
+import config from '../config'
+import './AddBookmark.css';
+
+const Required = () => (
+  <span className='AddBookmark__required'>*</span>
+)
 
 class AddBookmark extends Component {
-    render() { 
-        return (
-            <div className="addbookmark">
-                <h2>Add Bookmark!</h2>
-                <form className="addbookmark__form">
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" name="title" id="title" placeholder="Title"/>
-                    <label htmlFor="url">Url:</label>
-                    <input type="text" name="url" id="url" placeholder="url"/>
-                    <label htmlFor="description">Description:</label>
-                    <textarea name="description" id="description" placeholder="description"/>
-                    <label htmlFor="rating">Rating: </label>
-                    <input
-                        type="number"
-                        name="rating"
-                        id="rating"
-                        min="1"
-                        max="5"/>
-                    <div className="addbookmark__buttons">
-                        <button>Cancel</button>
-                        <button type="submit" >Save</button>
-                    </div>  
-                </form>
-            </div>
-        );
+  static contextType = BookmarksContext;
+
+  state = {
+    error: null,
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+    // get the form fields from the event
+    const { title, url, description, rating } = e.target
+    const bookmark = {
+      title: title.value,
+      url: url.value,
+      description: description.value,
+      rating: rating.value,
     }
+    this.setState({ error: null })
+    fetch(config.API_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify(bookmark),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          // get the error message from the response,
+          return res.json().then(error => {
+            // then throw it
+            throw error
+          })
+        }
+        return res.json()
+      })
+      .then(data => {
+        title.value = ''
+        url.value = ''
+        description.value = ''
+        rating.value = ''
+        this.context.addBookmark(data)
+        this.props.history.push('/')
+      })
+      .catch(error => {
+        this.setState({ error })
+      })
+  }
+
+  handleClickCancel = () => {
+    this.props.history.push('/')
+  };
+
+  render() {
+    const { error } = this.state
+    return (
+      <section className='AddBookmark'>
+        <h2>Create a bookmark</h2>
+        <form
+          className='AddBookmark__form'
+          onSubmit={this.handleSubmit}
+        >
+          <div className='AddBookmark__error' role='alert'>
+            {error && <p>{error.message}</p>}
+          </div>
+          <div>
+            <label htmlFor='title'>
+              Title
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              placeholder='Great website!'
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='url'>
+              URL
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='url'
+              name='url'
+              id='url'
+              placeholder='https://www.great-website.com/'
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='description'>
+              Description
+            </label>
+            <textarea
+              name='description'
+              id='description'
+            />
+          </div>
+          <div>
+            <label htmlFor='rating'>
+              Rating
+              {' '}
+              <Required />
+            </label>
+            <input
+              type='number'
+              name='rating'
+              id='rating'
+              defaultValue='1'
+              min='1'
+              max='5'
+              required
+            />
+          </div>
+          <div className='AddBookmark__buttons'>
+            <button type='button' onClick={this.handleClickCancel}>
+              Cancel
+            </button>
+            {' '}
+            <button type='submit'>
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  }
 }
- 
+
 export default AddBookmark;
---
+++ src/BookmarkItem/BookmarkItem.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Rating from '../Rating/Rating';
+import config from '../config';
+import BookmarksContext from '../BookmarksContext';
+import './BookmarkItem.css';
+
+function deleteBookmarkRequest(bookmarkId, cb) {
+  console.log(config.API_ENDPOINT + `/${bookmarkId}`)
+  fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
+    method: 'DELETE',
+    headers: {
+      'authorization': `bearer ${config.API_KEY}`
+    }
+  })
+  .then(res => {
+    if(!res.ok) {
+      return res.json().then(error => {
+        throw error
+      })
+    }
+    return res.json()
+  })
+  .then(data => {
+    cb(bookmarkId)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
+
+export default function BookmarkItem(props) {
+  return (
+    <BookmarksContext.Consumer>
+      {(context) => (
+        <li className='BookmarkItem'>
+          <div className='BookmarkItem__row'>
+            <h3 className='BookmarkItem__title'>
+              <a
+                href={props.url}
+                target='_blank'
+                rel='noopener noreferrer'>
+                {props.title}
+              </a>
+            </h3>
+            <Rating value={props.rating} />
+          </div>
+          <p className='BookmarkItem__description'>
+            {props.description}
+          </p>
+          <div className='BookmarkItem__buttons'>
+            <button
+              className='BookmarkItem__description'
+              onClick={() => {
+                deleteBookmarkRequest(
+                  props.id,
+                  context.deleteBookmark,
+                )
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      )}
+     </BookmarksContext.Consumer>
+  )
+}
+
+BookmarkItem.defaultProps = {
+  onClickDelete: () => {},
+}
